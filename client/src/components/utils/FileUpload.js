acc466a660a3fb5d1dd50a513c1ee963a215db00
백호동 <whitetiger@hashbrand.kr>
2021-01-09 15:16:28 +0900

    이미지 지우기

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -10,25 +10,30 @@ const FileUpload = () => {
 
   //파일 업로드를 핸들링
   const dropHander = (files) => {
-    //axios의 FormData를 통해 보내준다.
-    let formData = new FormData();
-
+    let formData = new FormData(); //axios의 FormData를 통해 보내준다.
     const config = {
       header: { "content-type": "multipart/form-data" },
     };
-    //formData 안에 append를 통해 파일들이 더해서 들어간다
-    formData.append("file", files[0]);
+    formData.append("file", files[0]); //formData 안에 append를 통해 파일들이 더해서 들어간다
 
     //axios를 통해 요청을 보내면 백앤드에서 파일을 저장하고 .then으로 파일저장정보를 받아온다.
     axios.post("/api/product/image", formData, config).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        // console.log(response.data);
         //기존 이미지를 넣어주고 새로 들어온 이미지를 추가해준다.
         setImages([...Images, response.data.filePath]);
       } else {
         alert("파일을 저장하는데 실패했습니다!");
       }
     });
+  };
+
+  //이미지 삭제하기
+  const deleteHandler = (image) => {
+    const currentIndex = Images.indexOf(image); //클릭한 이미지의 index를 찾는다.
+    const newImages = [...Images]; //기존이미지들을 복사
+    newImages.splice(currentIndex, 1); //클릭한 이미지의 인덱스 부터 1개를 지우겠다.(클릭한 이미지만 삭제)
+    setImages(newImages); //업데이트
   };
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -46,7 +51,6 @@ const FileUpload = () => {
             {...getRootProps()}
           >
             <input {...getInputProps()} />
-            {/* <p>Drag 'n' drop some files here, or click to select files</p> */}
             <Icon type="plus" style={{ fontSize: "3rem" }} />
           </div>
         )}
@@ -57,14 +61,11 @@ const FileUpload = () => {
           width: "350px",
           height: "240px",
           display: "flex",
-          //   alignItems: "center",
-          //   justifyContent: "center",
           overflowX: "scroll",
-          webkitScroll: "none",
         }}
       >
         {Images.map((image, index) => (
-          <div key={index}>
+          <div onClick={() => deleteHandler(image)} key={index}>
             <img
               style={{ minWidth: "300px", width: "300px", height: "240px" }}
               src={`http://localhost:5000/${image}`}
